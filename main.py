1fe57e93fb7816f4ed0fc5902335ad313479cbfb
nasir12041977 <nasir12041977@gmail.com>
2026-02-13 14:11:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
-import os
-from flask import Flask, request, jsonify
-from hyperliquid.exchange import Exchange
+from flask import Flask, jsonify
 from hyperliquid.info import Info
 from hyperliquid.utils import constants
-import eth_account
+import os
 
 app = Flask(__name__)
+address = os.getenv("HL_ADDRESS")
 
-# Render की सेटिंग्स से हम ये चाबियाँ उठाएंगे
-ADDRESS = os.getenv("HL_ADDRESS")
-SECRET_KEY = os.getenv("HL_SECRET_KEY")
-
-@app.route('/')
-def home():
-    return "<h1>Systumm Set Hai!</h1><p>Render engine is running.</p>"
-
-@app.route('/balance', methods=['GET'])
+@app.route('/balance')
 def get_balance():
     try:
-        info = Info(constants.MAINNET_API_URL, skip_ws=True)
-        user_state = info.spot_user_state(ADDRESS)
-        return jsonify(user_state)
+        info = Info(constants.MAINNET_API_URL)
+        state = info.spot_user_state(address)
+        return jsonify(state)
     except Exception as e:
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    # Render के लिए पोर्ट 10000 ज़रूरी है
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
